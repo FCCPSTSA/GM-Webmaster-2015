72d53131beef47fd69bd9f01846b052b704f5373
Erik Boesen <erik.boesen@gmail.com>
2015-11-10 12:11:58 -0500

    Animations!

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -10,7 +10,9 @@ $(window).scroll(function() {
 });
 $(document).ready(function() {
     $('h3').typed({
-        strings: ['A school equipped for the future!'],
-        typeSpeed: 50
+        strings: ['', 'A school equipped for the future!'],
+        typeSpeed: 50,
+        startDelay: 2000,
+        showCursor: false
     });
 });
