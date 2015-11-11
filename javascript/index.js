e8c2a4c1e2488dfdabf205a2ecaa1a7170213c7d
Erik Boesen <erik.boesen@gmail.com>
2015-11-11 18:54:54 -0500

    More style, more content, more everything! Except pages. Less pages. See counterpart website in repo GM-Webmaster-2015-Waste.

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
     $('h3').typed({
         strings: ['A school equipped for the future!'],
-        typeSpeed: 50,
+        typeSpeed: 10,
         startDelay: 2000,
         showCursor: false
+    });
+});
+$(window).scroll(function() {
+    var wScroll = $(window).scrollTop();
+    $('h1').css({
+        'transform': 'translate(-' + wScroll * 4 + 'px,0)',
+        'opacity': 1-wScroll/200
+    });
+    $('.jumbotron div').css({
+        'transform': 'rotate(-' + wScroll/10 + 'deg)',
+        'opacity': 1-wScroll/200
+    });
+    $('h3').css({
+        'transform': 'translate(' + wScroll * 4 + 'px,0)',
+        'opacity': 1-wScroll/200
     });
 });
