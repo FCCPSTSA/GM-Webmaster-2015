e986a997cb21dd6678382ec02e184722495410ac
Erik Boesen <erik.boesen@gmail.com>
2015-11-24 09:36:56 -0500

    More content and a picture.

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,10 +1,10 @@
-$(window).scroll(function() {
-    // Parallax effects
-    var wScroll = $(window).scrollTop();
-    $('.bg').css({
-        'transform': 'translate(0,-' + wScroll / 2 + 'px)'
-    });
-    $('header').css({
-        'background-color': 'rgba(100,100,100,' + wScroll/1000 + ')'
-    });
+$(window).scroll(function () {
+	// Parallax effects
+	var wScroll = $(window).scrollTop();
+	$('.bg').css({
+		'transform': 'translate(0,-' + wScroll / 2 + 'px)'
+	});
+	$('header').css({
+		'background-color': 'rgba(51,51,51,' + wScroll / 1000 + ')'
+	});
 });
